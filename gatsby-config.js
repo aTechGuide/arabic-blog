cc6e55a2ff1bef38ca73f5c5b25a8b9db7fd3e40
Kamran Ali <kamranalinitb@gmail.com>
2020-10-11 19:55:08 +0530

    updating gatsby-config and package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,8 @@ module.exports = {
           "script-src": "'self' 'unsafe-inline' www.google-analytics.com arabic-blog.disqus.com", //<- 'unsafe-inline' is unsafe and is required by Disqus
           "style-src": "'self' 'unsafe-inline' c.disquscdn.com", //<- "'unsafe-inline'" should be avoided but the plugin was broken with mergeStyleHashes
           "img-src": "'self' data: www.google-analytics.com referrer.disqus.com c.disquscdn.com",
-          "form-action": "'self' https://kamranali.us19.list-manage.com/subscribe/post?u=81033a93b0c17d2aca19ba835&amp;id=32d4ecb6e3"
+          "form-action": "'self' kamranali.us19.list-manage.com",
+          "connect-src": "'self' www.google-analytics.com"
         }
       }
     }
